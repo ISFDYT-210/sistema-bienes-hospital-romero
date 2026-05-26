67c6aa3915d94478d0518907c13c83e2db5cac8f
MeLlamoAgus98 <agusriple@hotmail.com>
2026-05-26 19:49:15 -0300

    Eliminar migraciones del build script para prevenir error Vercel

--- build_files.sh
+++ build_files.sh
@@ -6,5 +6,4 @@ python3 -m pip install -r requirements.txt --break-system-packages
 # Recolectar archivos estáticos
 python3 manage.py collectstatic --noinput --clear
 
-# Aplicar migraciones a la base de datos (Neon)
-python3 manage.py migrate --noinput
+# (Las migraciones se realizan localmente o fuera de Vercel para evitar errores de DDL con conexiones Pooled)
